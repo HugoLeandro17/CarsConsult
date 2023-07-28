8e6275d61d4c438c34073a8d6aac454d21990f17
Hugo Leandro <62100014+HugoLeandro17@users.noreply.github.com>
2023-07-28 23:13:33 +0100

    Initial commit

---
+++ src/main/java/com/hugoleandro/carsConsult/CarsConsultApplication.java
@@ -0,0 +1,13 @@
+package com.hugoleandro.carsConsult;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsConsultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsConsultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hugoleandro/carsConsult/CarsConsultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugoleandro.carsConsult;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsConsultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
